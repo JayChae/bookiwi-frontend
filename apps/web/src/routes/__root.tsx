812e31773265d1e5dea98aeb1484f80bfd879416
MR.J <cowhdals1111@gmail.com>
2025-04-11 16:09:23 +0900

    root head 태그 적용

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -1,9 +1,28 @@
-import { Outlet, createRootRoute } from "@tanstack/react-router";
+import { Outlet, createRootRoute, HeadContent } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
 export const Route = createRootRoute({
+  head: () => ({
+    meta: [
+      {
+        name: "description",
+        content: "소셜 이북 리더기, 함께 읽는 즐거움",
+      },
+      {
+        title: "Bookiwi",
+      },
+    ],
+    links: [
+      {
+        rel: "icon",
+        href: "/images/icon.png",
+      },
+    ],
+    scripts: [],
+  }),
   component: () => (
     <>
+      <HeadContent />
       <Outlet />
       <TanStackRouterDevtools />
     </>

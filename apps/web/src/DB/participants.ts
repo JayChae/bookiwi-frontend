10faf84f779577a0fee247192de75711f4e2900a
MR.J <cowhdals1111@gmail.com>
2025-04-12 00:31:04 +0900

    profiles popover

---
+++ apps/web/src/DB/participants.ts
@@ -0,0 +1,16 @@
+// Mock data for room participants
+const participants = [
+  {
+    id: "0",
+    name: "KIWI",
+    profileImage: "https://github.com/shadcn.png",
+    color: "green",
+  },
+  { id: "1", name: "조현지", profileImage: "", color: "red" },
+  { id: "2", name: "임진조", profileImage: "", color: "blue" },
+  { id: "3", name: "홍서하", profileImage: "", color: "yellow" },
+  { id: "4", name: "한상우", profileImage: "", color: "purple" },
+  { id: "5", name: "채종민", profileImage: "", color: "orange" },
+];
+
+export default participants;

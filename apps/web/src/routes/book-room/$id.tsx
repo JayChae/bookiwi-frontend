070e3730629372238a3b7cb207170b1be53fd5f0
MR.J <cowhdals1111@gmail.com>
2025-04-11 16:13:19 +0900

    Book Room 동적 title 적용

--- apps/web/src/routes/book-room/$id.tsx
+++ apps/web/src/routes/book-room/$id.tsx
@@ -4,18 +4,43 @@ import Header from "./-component/header";
 import Reader from "./-component/reader";
 import { ReaderProvider } from "./-reader";
 
+import bookRooms from "#/DB/book-room";
+
+interface BookRoomData {
+  id: string;
+  name: string;
+  description: string;
+  image: string;
+  lastActivityAt: string;
+}
+
 export const Route = createFileRoute("/book-room/$id")({
+  loader: async ({ params }) => {
+    // 실제로는 ID를 기반으로 책 정보를 API에서 가져오는 코드
+    const bookRoom = bookRooms.find(
+      (room: BookRoomData) => room.id === params.id,
+    );
+    return { bookRoom };
+  },
+  head: ({ loaderData }) => ({
+    meta: [
+      {
+        title: `Book Room | ${loaderData.bookRoom?.name}`,
+      },
+    ],
+  }),
   component: BookRoom,
 });
 
 function BookRoom() {
-  // const { id } = Route.useParams();
+  const { bookRoom } = Route.useLoaderData();
+  const bookTitle = bookRoom?.name || "Alice's Adventures in Wonderland";
 
   return (
     <main className="flex size-full h-screen w-screen flex-col overflow-y-hidden">
       <ReaderProvider>
         <Header
-          title="Alice's Adventures in Wonderland"
+          title={bookTitle}
           profileImage="https://github.com/shadcn.png"
           color="green"
         />
